56fc3601f70bb9b5e84b62d413747e8eada00fe5
Ian Jamieson <ian.jamieson@mi-c3.com>
2017-09-18 10:49:12 +0200

    table

---
+++ src/components/atoms/Table/TableHeaderColumn.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const Th = styled.th`
+
+`;
+
+export default Th;
---
+++ src/components/atoms/Table/index.js
@@ -0,0 +1,7 @@
+import Table from './Table';
+import Thead from './TableHeader';
+import Th from './TableHeaderColumn';
+import Tr from './TableRow';
+import Td from './TableRowColumn';
+
+export { Table, Thead, Th, Tr, Td };
