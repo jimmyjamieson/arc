a2c81315c573de8eb94b8ea8fe511958eb46eaef
Ian Jamieson <ian.jamieson@mi-c3.com>
2017-09-19 10:16:02 +0200

    manifest link fix

--- src/components/Html.js
+++ src/components/Html.js
@@ -16,7 +16,14 @@ const Html = ({ styles, assets, state, content }) => {
         {helmet.link.toComponent()}
         {assets.css.map(path => <link rel="stylesheet" type="text/css" key={path} href={path} />)}
         {styles}
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="manifest" href="/manifest/manifest.json" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/manifest/img/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/manifest/img/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/manifest/img/favicon-16x16.png" />
+        <link rel="mask-icon" href="/manifest/img/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="shortcut icon" href="/manifest/img/favicon.ico" />
+        <meta name="msapplication-config" content="/manifest/img/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body {...bodyAttrs}>
         <main id="app" dangerouslySetInnerHTML={{ __html: content }} />
